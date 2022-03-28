b759cc4e8b90f91543b21003ed3f81294bdb26d3
wasdisa <zekeriyaozurganci@gmail.com>
2022-03-28 04:27:48 +0300

    sl

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsari <42istanbul.com.tr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/13 09:58:42 by fsari             #+#    #+#             */
+/*   Updated: 2022/01/13 13:43:06 by fsari            ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*ft_get_line(char *save);
+char	*ft_save(char *save);
+char	*ft_read_and_save(int fd, char *save);
+char	*get_next_line(int fd);
+
+size_t	ft_strlen(char *str);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
